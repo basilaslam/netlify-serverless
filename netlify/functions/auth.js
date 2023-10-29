175dd22775c3ab171132998254f89f58c1f4e5b2
basilaslam <basilaslamnp@gmail.com>
2023-10-29 17:39:37 +0530

    Bug fix

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import serverless from 'serverless-http';
-
+import cors from 'cors'
 const app = express();
 
+const corsOptions = {
+  origin: 'https://visaa.vercel.app',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors())
 
 app.get('/.netlify/functions/auth/logout', (_, res) => {
   return res
